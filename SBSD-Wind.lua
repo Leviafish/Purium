c30a58d0aebf8fdf7b41665b77372e1f2e49a9cd
PuriumHuB <maiyeunguyenvietquoc@gmail.com>
2026-03-11 06:19:44 +0700

    Update SBSD-Wind.lua

--- SBSD-Wind.lua
+++ SBSD-Wind.lua
@@ -30,7 +30,7 @@ Window:EditOpenButton({
     CornerRadius = UDim.new(0,16),
     StrokeThickness = 2,
     Color = ColorSequence.new( -- ĐÃ ĐỔI SANG GRADIENT TRẮNG ĐEN
-        Color3.fromHex("30, 30, 30"), 
+        Color3.fromHex("1E1E1E"), 
         Color3.fromHex("000000")
     ),
     OnlyMobile = false,
@@ -383,4 +383,5 @@ ThemeSection:Keybind({
 })
 
 print("successfully loaded all asset!")
+
 
